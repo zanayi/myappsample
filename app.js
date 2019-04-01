316e0bb6b8674016767ea5d1d1a6ebac68d9281d
Lohty <Lohty@gmail.com>
2019-04-01 19:33:43 +0700

    latihan

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+//DOM SELECTION
+
+//document.getElementById() mengembalikan element
+
+const judul = document.getElementById('judul');
+
+
+//document.getElementsByTagName() mengembalikan HTMLCollention
+const p = document.getElementsByTagName('p');
+
+for(let i = 0; i < p.length; i++){
+    p[i].style.backgroundColor = 'lightblue';
+}
+
+
+//document.getelementsByClassName() mengembalikan HTMLCollention
+
+const p1 = document.getElementsByClassName('p1');
